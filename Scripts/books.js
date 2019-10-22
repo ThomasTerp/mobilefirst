a0824967a33c2ef0cbc9bd5262ff3f102dac4d1e
ThomasTerp <thomascterp@gmail.com>
2019-10-22 15:48:54 +0200

    Initial commit

---
+++ Scripts/books.js
@@ -0,0 +1,106 @@
+
+$(window).on("load", () =>
+{
+    class Book
+	{
+		constructor(id, schoolID, name, description, imgSrc, guidedPrice, price)
+		{
+			this.id = id;
+			this.schoolID = schoolID;
+			this.name = name;
+            this.description = description;
+            this.imgSrc = imgSrc
+			this.guidedPrice = guidedPrice;
+			this.price = price;
+        }
+        
+        createHTML()
+        {
+            this.html = $(`
+                <a class="book" href="index.html">
+                    <img class="product" src="${this.imgSrc}" />
+                    <img class="cart" src="Frontend/Images/Shop/Cart.png" />
+                    <span class="id">Varenummer: ${this.id}</span>
+                    <span class="name">${this.name}</span>
+                    <span class="description">${truncate(this.description, 100)}</span>
+                    <span class="guided-price">Vejledende pris: ${this.guidedPrice.toFixed(2)} DKK</span>
+                    <span class="price">Din pris: ${this.price.toFixed(2)} DKK</span>
+                </a>
+            `);
+            
+            return this.html;
+        }
+    }
+    
+    let books = [
+        new Book(619519113, "aalborgsosunord", "Grundforløb 2. PAU", "Grundforløb 2. PAU Vibeke Mossing Thorenfeldt;Mette Vang;Trine Hartmann Grosen;Maria Månsson;Elin Kristensen;Lene Rose Andersen;Mette la Cour Sell;Dorthe Nikolajsen;Rikke Erbs Nørgaard Munksgaard,2015 Sider : 303 ISBN : 978876281510 ", "Frontend/Images/Shop/1.png", 285, 228),
+        new Book(343738535, "aalborgsosunord", "Grundforløb 2. Sosu", "Grundforløb 2. Sosu Anne Katrine Ørnstrup;Vibeke Mossing Thorenfeldt;Mette Vang;Dorthe Brix;Tina Rode Melbye;Trine Hartmann Grosen;Elin Kristensen Munksgaard,2017 Sider : 480 ISBN : 9788762817876", "Frontend/Images/Shop/2.png", 530, 424),
+        new Book(295566986, "aalborgsosunord", "Naturfag. Niveau F og E", "Naturfag. Niveau F og E Henrik Andersen;Ole Bjerglund Pedersen;Vian Bech Munksgaard,2015 Sider : 160 ISBN : 9788762815766", "Frontend/Images/Shop/5.png", 285, 228),
+        new Book(210869980, "hjorringsosunord", "Grundforløb 2. Sosu", "Grundforløb 2. Sosu Anne Katrine Ørnstrup;Vibeke Mossing Thorenfeldt;Mette Vang;Dorthe Brix;Tina Rode Melbye;Trine Hartmann Grosen;Elin Kristensen Munksgaard,2017 Sider : 480 ISBN : 9788762817876", "Frontend/Images/Shop/3.png", 530, 242),
+        new Book(558498482, "hjorringsosunord", "Naturfag. Niveau F og E", "Naturfag. Niveau F og E Henrik Andersen;Ole Bjerglund Pedersen;Vian Bech Munksgaard,2015 Sider : 160 ISBN : 9788762815766", "Frontend/Images/Shop/4.png", 285, 228),
+        new Book(971716090, "vendsyssel", "Aktiviteter, kultur og natur. Pædagogisk assistent", "Aktiviteter, kultur og natur. Pædagogisk assistent Alis Jensen;Jakob Freud-Magnus;Elsi Busk Odderup;Anette Juhl;Agnete Gudnason;Marie Weber-Andersen;John Nilsson;Linda Katja Jensen", "Frontend/Images/Shop/1.png", 460, 368),
+        new Book(678481604, "vendsyssel", "Børn og natur", "Børn og natur Ophelia Achton Frydenlund,2013 Sider : 112 ISBN : 9788771180848", "Frontend/Images/Shop/1.png", 199, 368),
+        new Book(215195014, "vendsyssel", "Kommunikation og digital kultur. Pædagogisk assistent", "Kommunikation og digital kultur. Pædagogisk assistent Alis Jensen;Trine Reinholdt Gath;Henrik Wiben;Anja Semke;Lena Knudsmark Munksgaard,2016 Sider : 278 ISBN : 9788762815148", "Frontend/Images/Shop/3.png", 330, 264),
+        new Book(295125530, "thistedsosu", "Danskbogen til social- og sundhedselever 2. udgave", "Danskbogen til social- og sundhedselever 2. udgave Lisbeth B. Jensen, Jesper Hansen, Aya Køllgaard Carlsen, Birgitte Andersen Gads Forlag,2015 Sider : 240 ISBN : 9788712050803", "Frontend/Images/Shop/5.png", 250, 187.50),
+        new Book(847143853, "thistedsosu", "Håndbog i blodprøver", "Håndbog i blodprøver Henrik Andersen;Cheme Andersen Munksgaard,2014 Sider : 135 ISBN : 9788762814189", "Frontend/Images/Shop/5.png", 145, 116),
+        new Book(438224481, "thistedsosu", "Håndbog i lægemidler", "Håndbog i lægemidler Inge Olsen Munksgaard,2014 Sider : 164 ISBN : 9788762812970", "Frontend/Images/Shop/4.png", 175, 140),
+        new Book(638534476, "thistedsosu", "Mødet med borgeren (SSH)", "Mødet med borgeren (SSH) Jette Nielsen;Maria Kehlet Brockhoff;Henrik Wiben;Jytte Solhøj;Anja Semke;Birgitte Gøtzsche;Ulla Lund Eskildsen;Merete Becker-Larsen Munksgaard,2017 Sider : 164 ISBN : 9788762817722", "Frontend/Images/Shop/2.png", 195, 156),
+        new Book(141393062, "thistedsosu", "Personlig hjælp, omsorg og pleje (SSH)", "Personlig hjælp, omsorg og pleje (SSH) Jette Nielsen;Vibeke Mossing Thorenfeldt;Henrik Wiben;Dorthe Brix;Tina Rode Melbye;Anja Semke;Birgitte Gøtzsche;Trine Hartmann Grosen;Børge Hede;Rikke Syberg Kjær;Anne...", "Frontend/Images/Shop/3.png", 590, 472),
+        new Book(788168522, "herningsosu", "Samfundsfag, Niveau C - Grundbog", "Samfundsfag, Niveau C - Grundbog Hans Henrik Jacobsen, Jørgen Sauer Trojka,2015 Sider : 0 ISBN : 9788771540222", "Frontend/Images/Shop/2.png", 268, 201),
+        new Book(767759415, "herningsosu", "Sundhed, krop og bevægelse. Pædagogisk assistent", "Sundhed, krop og bevægelse. Pædagogisk assistent Anne-Merete Kissow;Mogens Ingelhardt Hansen;Henrik Wiben;Anja Semke;Michaela Jørgensen;Anna Østergaard Lauritsen;Pernille", "Frontend/Images/Shop/4.png", 635, 508),
+        new Book(210869980, "kobenhavnsuniversistet", "Grundforløb 2. Sosu", "Grundforløb 2. Sosu Anne Katrine Ørnstrup;Vibeke Mossing Thorenfeldt;Mette Vang;Dorthe Brix;Tina Rode Melbye;Trine Hartmann Grosen;Elin Kristensen Munksgaard,2017 Sider : 480 ISBN : 9788762817876", "Frontend/Images/Shop/4.png", 530, 242),
+        new Book(847143853, "kobenhavnsuniversistet", "Håndbog i blodprøver", "Håndbog i blodprøver Henrik Andersen;Cheme Andersen Munksgaard,2014 Sider : 135 ISBN : 9788762814189", "Frontend/Images/Shop/3.png", 145, 116),
+        new Book(788168522, "kobenhavnsuniversistet", "Samfundsfag, Niveau C - Grundbog", "Samfundsfag, Niveau C - Grundbog Hans Henrik Jacobsen, Jørgen Sauer Trojka,2015 Sider : 0 ISBN : 9788771540222", "Frontend/Images/Shop/2.png", 268, 201),
+        new Book(619519113, "kobenhavnsuniversistet", "Grundforløb 2. PAU", "Grundforløb 2. PAU Vibeke Mossing Thorenfeldt;Mette Vang;Trine Hartmann Grosen;Maria Månsson;Elin Kristensen;Lene Rose Andersen;Mette la Cour Sell;Dorthe Nikolajsen;Rikke Erbs Nørgaard Munksgaard,2015 Sider : 303 ISBN : 9788762815100", "Frontend/Images/Shop/5.png", 285, 228)
+    ];
+    
+    //Get a parameter from the URL
+	function GetURLParameter(param)
+	{
+		var pageURL = window.location.search.substring(1);
+		var urlVariables = pageURL.split("&");
+		
+		for (var i = 0; i < urlVariables.length; i++)
+		{
+			var parameterName = urlVariables[i].split("=");
+			
+			if (parameterName[0] == param)
+			{
+				return parameterName[1];
+			}
+		}
+    }
+    
+    function truncate(text, max)
+    {
+        if(text.length > max)
+        {
+           return text.substring(0, max) + "...";
+        }
+        else
+        {
+           return text;
+        }
+     };
+    
+    const educationID = GetURLParameter("education");
+    const schoolID = GetURLParameter("school");
+    
+    if(typeof educationID !== "undefined" && typeof schoolID !== "undefined")
+    {
+        //Filter books by school from URL parameter
+        books = books.filter((book) =>
+        {
+            return book.schoolID === schoolID;
+        });
+        
+        books.forEach((book) =>
+        {
+            $("#BooksDiv").append(book.createHTML());
+        });
+    }
+    else
+    {
+        location.href = "index.html";
+    }
+});
---
+++ Scripts/schools.js
@@ -0,0 +1,189 @@
+
+$(window).on("load", () =>
+{
+    class School
+    {
+        constructor(id, name, coordinates, books)
+        {
+            this.id = id;
+            this.name = name;
+            this.coordinates = coordinates;
+            this.books = books;
+        }
+        
+        createHTML()
+        {
+            this.html = $(`
+                <a class="school" href="books.html?education=${GetURLParameter("education")}&school=${this.id}">
+                    ${this.name}
+                </a>
+            `);
+            
+            return this.html;
+        }
+    }
+    
+    const schools = [
+		new School("aalborgsosunord", "Aalborg, SOSU Nord", {latitude: 57.041864, longitude: 9.920778}),
+		new School("hjorringsosunord", "Hjørring, SOSU Nord", {latitude: 57.458418, longitude: 10.022469}),
+		new School("vendsyssel", "Vendsyssel", {latitude: 57.507089, longitude: 10.279546}),
+		new School("thistedsosu", "Thisted, SOSU", {latitude: 56.965826, longitude: 8.704670}),
+		new School("herningsosu", "Herning, SOSU", {latitude: 56.142369, longitude: 8.969105}),
+		new School("kobenhavnsuniversistet", "Københavns Universitet", {latitude: 55.680191, longitude: 12.572345})
+	];
+    
+    //Get a parameter from the URL
+	function GetURLParameter(param)
+	{
+		var pageURL = window.location.search.substring(1);
+		var urlVariables = pageURL.split("&");
+		
+		for (var i = 0; i < urlVariables.length; i++)
+		{
+			var parameterName = urlVariables[i].split("=");
+			
+			if (parameterName[0] == param)
+			{
+				return parameterName[1];
+			}
+		}
+	}
+    
+    function rad(x)
+	{
+		return x * Math.PI / 180;
+	}
+    
+    //Get distance between two coordinates using the Haversine formula
+	function getDistance(coordinates1, coordinates2)
+	{
+		var R = 6378137;
+		var dLat = rad(coordinates2.latitude - coordinates1.latitude);
+		var dLong = rad(coordinates2.longitude - coordinates1.longitude);
+		var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(coordinates1.latitude)) * Math.cos(rad(coordinates2.latitude)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		var d = R * c;
+		
+		return d;
+	}
+    
+    //Ask user to use Google maps to get coordinates
+    function useGoogleMaps()
+    {
+        $("#ChooseSchoolSection > h1").text("Indlæser Google Maps...");
+        $("#SchoolsDiv").empty();
+        
+        let checkMapsAvailabilityInterval;
+        
+        const checkMapsAvailability = () =>
+        {
+            if(typeof google === "object" && typeof google.maps === "object")
+            {
+                $("#Maps").show();
+                
+                const map = new google.maps.Map($("#Maps")[0], {
+                    center: {
+                        lat: 56.290451,
+                        lng: 10.447101
+                    },
+                    zoom: 8
+                });
+                
+                let hasClicked = false;
+                
+                google.maps.event.addListener(map, "click", (event) =>
+                {
+                    if(!hasClicked)
+                    {
+                        const coordinates = {
+                            latitude: event.latLng.lat(),
+                            longitude: event.latLng.lng()
+                        };
+                        
+                        new google.maps.Marker({
+                            position: event.latLng,
+                            map: map,
+                            title: "Din placering"
+                        });
+                        
+                        setTimeout(() =>
+                        {
+                            gotCoordinates(coordinates);
+                        }, 1000);
+                        
+                        hasClicked = true;
+                    }
+                });
+                
+                $("#ChooseSchoolSection > h1").text("Hvor er du?");
+                
+                clearInterval(checkMapsAvailabilityInterval);
+            }
+        };
+        checkMapsAvailabilityInterval = setInterval(checkMapsAvailability, 100);
+        checkMapsAvailability();
+    };
+    
+    //Sucessfully got coordinates from user
+    function gotCoordinates(coordinates)
+    {
+        $("#Maps").empty();
+        $("#Maps").hide();
+        $("#ChooseSchoolSection > h1").text("Hvilken skole skal du gå på?");
+        
+        //Sort schools by distance to user
+        schools.sort((school1, school2) =>
+        {
+            return getDistance(coordinates, school1.coordinates)-getDistance(coordinates, school2.coordinates);
+        });
+        
+        for(let schoolIndex = 0; schoolIndex < 4; schoolIndex++)
+        {
+            const school = schools[schoolIndex];
+            
+            $("#SchoolsDiv").append(school.createHTML());
+        }
+        
+        const selectOtherCoordinatesHTML = $(`
+            <div class="school">
+                Vælg en anden placering
+            </div>
+        `);
+        selectOtherCoordinatesHTML.on("click", (even) =>
+        {
+            useGoogleMaps();
+        });
+        $("#SchoolsDiv").append(selectOtherCoordinatesHTML);
+    };
+    
+    if(typeof GetURLParameter("education") !== "undefined")
+    {
+        //If the geolocation api is available
+        if("geolocation" in navigator)
+        {
+            const options = {
+                maximumAge: 30000,
+                timeout: 27000,
+                enableHighAccuracy: true
+            };
+            const successCallback = (coordinates) =>
+            {
+                gotCoordinates(coordinates);
+            }
+            const errorCallback = () =>
+            {
+                useGoogleMaps();
+            }
+            
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+        }
+        else
+        {
+            useGoogleMaps();
+        }
+    }
+    else
+    {
+        location.href = "index.html";
+    }
+});
